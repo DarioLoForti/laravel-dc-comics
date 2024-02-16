eaca0b5d5e55dd3adf186daad22890010f9a3e36
DarioLoForti <lofortidario@gmail.com>
2024-02-16 15:11:20 +0100

    creo tabella sul db tramite migrations

--- database/migrations/2024_02_16_135022_create_comics_table.php
+++ database/migrations/2024_02_16_135022_create_comics_table.php
@@ -15,6 +15,13 @@ return new class extends Migration
     {
         Schema::create('comics', function (Blueprint $table) {
             $table->id();
+            $table->string('title', 50);
+            $table->text('description', 250);
+            $table->string('thumb', 255)->nullable();
+            $table->string('price', 10);
+            $table->string('series', 50);
+            $table->date('sale_date');
+            $table->string('type', 50);
             $table->timestamps();
         });
     }
