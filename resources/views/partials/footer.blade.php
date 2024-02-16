8dfa908447fd8374bb8829a9a6b57285501c2634
DarioLoForti <lofortidario@gmail.com>
2024-02-16 15:03:36 +0100

    importo layaut base

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,99 @@
+<footer>
+    <div class="bg">
+
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <div class="appbar d-flex">
+                        <img src="{{ Vite::asset('resources/img/buy-comics-digital-comics.png') }}" alt="">
+                        <h2>Digital Comics</h2>
+                        <img src="{{ Vite::asset('resources/img/buy-comics-merchandise.png') }}" alt="">
+                        <h2>Dc merchandise</h2>
+                        <img src="{{ Vite::asset('resources/img/buy-comics-subscriptions.png') }}" alt="">
+                        <h2>Subscription</h2>
+                        <img src="{{ Vite::asset('resources/img/buy-comics-shop-locator.png') }}" alt="">
+                        <h2>Comic shop locator</h2>
+                        <img src="{{ Vite::asset('resources/img/buy-dc-power-visa.svg') }}" alt="">
+                        <h2>Dc power visa</h2>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <div class="row">
+
+        </div>
+        <div class="top-footer">
+            <div class="list-footer">
+                <h3> dc comics</h3>
+                <ul>
+                    <li>characters</li>
+                    <li>comics</li>
+                    <li>movies</li>
+                    <li>tv</li>
+                    <li>games</li>
+                    <li>videos</li>
+                    <li>news</li>
+                </ul>
+                <h3>shop</h3>
+                <ul>
+                    <li>shop dc</li>
+                    <li>shop dc collectibles</li>
+                </ul>
+            </div>
+            <div class="list-footer">
+                <h3>dc</h3>
+                <ul>
+                    <li>terms of us</li>
+                    <li>privacy policy (new)</li>
+                    <li>ad choise</li>
+                    <li>advertising</li>
+                    <li>jobs</li>
+                    <li>subscription</li>
+                    <li>talent workshops</li>
+                    <li>cpsc certificates</li>
+                    <li>ratings</li>
+                    <li>shop help</li>
+                    <li>contact us</li>
+                </ul>
+            </div>
+            <div class="list-footer">
+                <h3>sites</h3>
+                <ul>
+                    <li>dc</li>
+                    <li>mad magazine</li>
+                    <li>dc kids</li>
+                    <li>dc universe</li>
+                    <li>dc power visa</li>
+                </ul>
+            </div>
+        </div>
+
+    </div>
+    <div class="bg-footer">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <div class="content">
+                        <button> sign-up now!</button>
+                        <div class="image">
+                            <ul>
+                                <li><img src="{{ Vite::asset('resources/img/footer-facebook.png') }}" alt="">
+                                </li>
+                                <li><img src="{{ Vite::asset('resources/img/footer-twitter.png') }}" alt="">
+                                </li>
+                                <li><img src="{{ Vite::asset('resources/img/footer-youtube.png') }}" alt="">
+                                </li>
+                                <li><img src="{{ Vite::asset('resources/img/footer-pinterest.png') }}" alt="">
+                                </li>
+                                <li><img src="{{ Vite::asset('resources/img/footer-periscope.png') }}" alt="">
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,47 @@
+<header>
+
+    <div class="container">
+        <div class="navbar">
+            <div class="logo">
+                <img src="{{ Vite::asset('resources/img/dc-logo.png') }}" alt="Logo">
+            </div>
+            <div class="menu">
+                <ul class="list-unstyled d-flex">
+                    <li>
+                        <a class="{{ Route::currentRoutename() === 'home' ? 'navtext' : '' }}"
+                            href="{{ route('home') }}">Characters</a>
+                    </li>
+                    <li>
+                        <a class="{{ Route::currentRoutename() === 'comics' ? 'navtext' : '' }}"
+                            href="#">Comics</a>
+                    </li>
+                    <li>
+                        <a class="{{ Route::currentRoutename() === 'movies' ? 'navtext' : '' }}"
+                            href="#">Movies</a>
+                    </li>
+                    <li>
+                        tv
+                    </li>
+                    <li>
+                        Games
+                    </li>
+                    <li>
+                        Collectibles
+                    </li>
+                    <li>
+                        Videos
+                    </li>
+                    <li>
+                        Fans
+                    </li>
+                    <li>
+                        News
+                    </li>
+                    <li>
+                        Shop
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</header>
