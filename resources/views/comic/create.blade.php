e0529c0f575bf6045dd8b582bac309e807a9d258
DarioLoForti <lofortidario@gmail.com>
2024-02-19 14:23:32 +0100

    tramite metodo store aggiungo nuovo elemento alla tabella nel db

--- resources/views/comic/create.blade.php
+++ resources/views/comic/create.blade.php
@@ -19,12 +19,12 @@
                         <div class="form-group">
                             <label for="description">Description</label>
                             <textarea name="description" class="form-control" id="description" cols="30" rows="10"
-                                placeholder="Add description"></textarea>
+                                placeholder="Add description" required></textarea>
                         </div>
                         <div class="form-group">
                             <label for="thump">Thumb</label>
                             <input type="text" class="form-control" name="thumb" id="thumb"
-                                placeholder="Add URL image" required>
+                                placeholder="Add URL image">
                         </div>
                         <div class="form-group">
                             <label for="price">Price</label>
