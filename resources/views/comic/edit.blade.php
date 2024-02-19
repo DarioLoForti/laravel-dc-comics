3a3207617297f8c95f5a9f559aa6e308a3717f63
DarioLoForti <lofortidario@gmail.com>
2024-02-19 15:54:14 +0100

    esercizio più bonus completato

--- resources/views/comic/edit.blade.php
+++ resources/views/comic/edit.blade.php
@@ -4,7 +4,7 @@
     <div class="container">
         <div class="row">
             <div class="col 12">
-                <h2 class="text-white text-center">Edit Comic</h2>
+                <h2 class="text-white text-center mt-4">Edit Comic</h2>
             </div>
             <div class="col-12">
                 <div class="form-container d-flex">
