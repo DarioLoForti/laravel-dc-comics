77d2741d3ef1713604cb1a927d8b189f6725dfee
DarioLoForti <lofortidario@gmail.com>
2024-02-19 15:27:48 +0100

    inserisco valore di dafault originale nella form di modifica

--- resources/views/comic/edit.blade.php
+++ resources/views/comic/edit.blade.php
@@ -15,37 +15,37 @@
                         <div class="form-group">
                             <label for="title">Title</label>
                             <input type="text" class="form-control" name="title" id="title" placeholder="Add title"
-                                required>
+                                value="{{ $comic->title }}" required>
                         </div>
                         <div class="form-group">
                             <label for="description">Description</label>
                             <textarea name="description" class="form-control" id="description" cols="30" rows="10"
-                                placeholder="Add description" required></textarea>
+                                placeholder="Add description" value="{{ $comic->description }}" required></textarea>
                         </div>
                         <div class="form-group">
                             <label for="thump">Thumb</label>
                             <input type="text" class="form-control" name="thumb" id="thumb"
-                                placeholder="Add URL image">
+                                placeholder="Add URL image" value="{{ $comic->thumb }}">
                         </div>
                         <div class="form-group">
                             <label for="price">Price</label>
                             <input type="text" class="form-control" name="price" id="price" placeholder="Add price"
-                                required>
+                                value="{{ $comic->price }}" required>
                         </div>
                         <div class="form-group">
                             <label for="series">Series</label>
                             <input type="text" class="form-control" name="series" id="series"
-                                placeholder="Add series" required>
+                                placeholder="Add series" value="{{ $comic->series }}" required>
                         </div>
                         <div class="form-group">
                             <label for="sale_date">Sale Date</label>
                             <input type="text" class="form-control" name="sale_date" id="sale_date"
-                                placeholder="Add sale date" required>
+                                placeholder="Add sale date" value="{{ $comic->sale_date }}" required>
                         </div>
                         <div class="form-group">
                             <label for="type">Type</label>
                             <input type="text" class="form-control" name="type" id="type" placeholder="Add type"
-                                required>
+                                value="{{ $comic->type }}" required>
                         </div>
                         <div class="form-group mb-4">
                             <button type="submit" class="btn btn-success">Save</button>
